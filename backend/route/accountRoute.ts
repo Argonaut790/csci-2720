19bd0d573e14b366eda73f1c00f2a75dc84adcd3
lalalam123 <90650499+lalalam123@users.noreply.github.com>
2023-12-13 21:50:21 +0800

    update backend CRUD: account fav & data id price

--- backend/route/accountRoute.ts
+++ backend/route/accountRoute.ts
@@ -1,61 +1,72 @@
 import express, { Request, Response } from "express";
 
 const router = express.Router();
-const Account = require("../model/account");
+import Account from "../model/account";
 
 var multer = require("multer");
 var upload = multer();
 
 // Login
 router.post("/login", upload.array(), async (req: Request, res: Response) => {
-  const user = await Account.findOne({ email: req.body.email });
-
-  if (!user) {
-    res.status(400);
-    res.send("Wrong email / password");
-  } else {
-    if (req.body.password === user.password) {
-      res.status(200).json({
+  try {
+    const user = await Account.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(401).send("Incorrect email and password");
+    } else if (user && user.password && req.body.password === user.password) {
+      const userToken = {
         userId: user.userId,
         username: user.username,
         isAdmin: user.isAdmin,
-      });
-      // }
+        email: user.email,
+        favourite: user.favourite,
+      };
+      res
+        .status(200)
+        .json(userToken)
+        .send("Success login as " + user.username);
     } else {
-      res.status(404).send("Wrong password");
+      res.status(401).send("Wrong password");
     }
+  } catch (err) {
+    res.status(500).send("Server Error");
   }
 });
 
 // Sign up
 router.post("/signup", upload.array(), async (req, res) => {
-  // Server-side validation
-  const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  if (!req.body || !req.body.email || !req.body.email.match(emailRegEx)) {
-    return res.status(401).json("Invalud email");
-  }
-
-  const registeredUser = await Account.find({ email: req.body.email });
-
-  // Email Registered
-  if (registeredUser.length) {
-    return res
-      .status(400)
-      .send("This email has been registered. Please click 'Forgot Password' if you cannot login.");
-  }
-
-  const account = new Account({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-  });
-
   try {
+    // Server-side validation
+    const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!req.body || !req.body.email || !req.body.email.match(emailRegEx)) {
+      return res.status(401).send("Invalud email");
+    }
+
+    const registeredUser = await Account.find({ email: req.body.email });
+
+    // Email Registered
+    if (registeredUser.length) {
+      return res
+        .status(400)
+        .send(
+          "This email has been registered. Please click 'Forgot Password' if you cannot login."
+        );
+    }
+
+    const account = new Account({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      isAdmin: false,
+      favourite: [],
+    });
     const saveduser = await account.save();
-    res.status(200).json(saveduser);
+    res
+      .status(200)
+      .json(saveduser)
+      .send("Success sign up as " + req.body.username);
   } catch (err) {
-    res.status(401).json("Unknown error, please try again or sign up with another method.");
+    res.status(500).send("Unknown error, please try again or sign up with another method.");
   }
 });
 
@@ -63,28 +74,29 @@ router.post("/signup", upload.array(), async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const list = await Account.find();
-    res.json(list);
+    res.status(200).json(list).send("Success get all user info");
   } catch (err) {
-    res.json({ message: err });
+    res.status(500).json({ message: err }).send("Failed get all user info");
   }
 });
 
 // Get one user info
 router.get("/:userId", async (req, res) => {
-  console.log("Gettin user info");
   try {
     const user = await Account.findOne({ userId: req.params.userId });
     console.log(user);
-    res.json(user);
+    res
+      .status(200)
+      .json(user)
+      .send("Success get one user info" + user?.username);
   } catch (err) {
     console.log(err);
-    res.json({ message: err });
+    res.status(500).json({ message: err }).send("Failed get one user info");
   }
 });
 
 // Update user info
 router.patch("/:userId", async (req, res) => {
-  console.log("Updating user info");
   try {
     const updatedUser = await Account.updateOne(
       { userId: req.params.userId },
@@ -96,38 +108,44 @@ router.patch("/:userId", async (req, res) => {
       }
     );
     console.log(updatedUser);
-    res.json(updatedUser);
+    res
+      .status(200)
+      .json(updatedUser)
+      .send("Success update user info" + req.body.username);
   } catch (err) {
-    res.json({ message: err });
+    res.status(500).json({ message: err }).send("Failed update user info");
   }
 });
 
 // Delete user
 router.delete("/:userId", async (req, res) => {
-  console.log("Deleting user");
   try {
     const removedUser = await Account.deleteOne({ userId: req.params.userId });
     console.log(removedUser);
-    res.json(removedUser);
+    res
+      .status(200)
+      .json(removedUser)
+      .send("Success delete user" + req.body.username);
   } catch (err) {
-    res.json({ message: err });
+    res.status(500).json({ message: err }).send("Failed delete user");
   }
 });
 
 // Admin Add user
 router.post("/", async (req, res) => {
-  console.log("Adding user");
-  const account = new Account({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-  });
-
   try {
+    console.log("Adding user");
+    const account = new Account({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      isAdmin: req.body.isAdmin,
+      favourite: [],
+    });
     const saveduser = await account.save();
-    res.json(saveduser);
+    res.status(200).json(saveduser).send("Success add new admin user");
   } catch (err) {
-    res.json({ message: err });
+    res.status(500).json({ message: err }).send("Failed add new admin user");
   }
 });
 
